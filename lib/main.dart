01f7aefc3373d30f0f4e4970e5ecd86ac7ee0b84
augustob790 <augustosousa790@gmail.com>
2020-01-05 18:13:58 -0300

    App Gasolina ou Alcool

---
+++ lib/main.dart
@@ -0,0 +1,10 @@
+import 'package:app_alcool_e_gasolina/home.dart';
+import 'package:flutter/material.dart';
+
+void main(){
+  runApp(MaterialApp(
+    home: Home() ,
+    debugShowCheckedModeBanner: false,
+  )
+  );
+}
